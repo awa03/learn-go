e25ecd4bc6963d03c978baf3c862f7a85b92b86b
Erik Dubois <awa22@fsu.edu>
2024-10-07 11:45:37 -0400

    Init Audio

---
+++ examples/audio/go.mod
@@ -0,0 +1,5 @@
+module audio
+
+go 1.23.0
+
+require github.com/gen2brain/malgo v0.11.22 // indirect
---
+++ examples/audio/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "fmt"
+	"github.com/gen2brain/malgo"
+)
+
+func main(){
+}
