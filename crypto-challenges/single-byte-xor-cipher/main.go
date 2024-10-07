1f792d5ff2375250cc7e14c8692a870cfe14d021
Erik Dubois <awa22@fsu.edu>
2024-10-06 21:37:45 -0400

    Add Comments

--- crypto-challenges/single-byte-xor-cipher/main.go
+++ crypto-challenges/single-byte-xor-cipher/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func scoreText(text []byte) float64 {
+  // Score based on frequency in alphabet
 	frequency := map[byte]float64{
 		' ': 13.0, 'E': 12.7, 'T': 9.1, 'A': 8.2, 'O': 7.5, 'I': 7.0, 'N': 6.7, 'S': 6.3, 'H': 6.1, 'R': 6.0,
 		'D': 4.3, 'L': 4.0, 'U': 2.8, 'C': 2.8, 'M': 2.4, 'F': 2.2, 'Y': 2.0, 'W': 2.0, 'G': 2.0, 'P': 1.9,
@@ -27,6 +28,7 @@ func scoreText(text []byte) float64 {
 
 func xorWithKey(buf [] byte, key byte) []byte {
   result := make([]byte, len(buf))
+  // xor each byte
   for i:=range buf {
     result[i] = buf[i] ^ key
   }
